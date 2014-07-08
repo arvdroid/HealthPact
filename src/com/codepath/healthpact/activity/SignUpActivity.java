b798ded1f252013fa026f700916a82b872989f93
Venkata Dantuluri <dvnraju2003@gmail.com>
2014-07-08 00:21:24 -0700

    adding login and sign up screens

---
+++ src/com/codepath/healthpact/activity/SignUpActivity.java
@@ -0,0 +1,20 @@
+package com.codepath.healthpact.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.codepath.healthpact.R;
+
+public class SignUpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sign_up);
+	}
+	
+	public void onHomeView(View v) {
+
+	}
+}
