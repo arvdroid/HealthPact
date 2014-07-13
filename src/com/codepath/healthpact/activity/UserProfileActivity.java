4dfad11d5ce058b7f495301d2af274a48ee4a81f
arvdroid <amudun@gmail.com>
2014-07-12 19:10:28 -0700

    update user profile view

---
+++ src/com/codepath/healthpact/activity/UserProfileActivity.java
@@ -0,0 +1,16 @@
+package com.codepath.healthpact.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.codepath.healthpact.R;
+
+public class UserProfileActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_profile);
+	}
+
+}
