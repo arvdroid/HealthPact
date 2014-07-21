1aa791daffd52152fddd63a4392dc3a0b7493317
dipankar14 <dipankar14@hotmail.com>
2014-07-20 20:16:31 -0700

    Action.java changes with get getter/setter

--- src/com/codepath/healthpact/models/Action.java
+++ src/com/codepath/healthpact/models/Action.java
@@ -4,23 +4,52 @@ import com.parse.ParseClassName;
 import com.parse.ParseObject;
 
 @ParseClassName("Action")
-public class Action extends ParseObject{
+public class Action extends ParseObject {
 	private boolean updated;
 	private int progress;
-	
+
 	public Action() {
 		super();
 	}
-	
-	public void setActionName(String name) {put("action_name", name);}
 
-	public String getActionName() {return getString("action_name");}
+	public int getProgress() {
+		return progress;
+	}
 
-	public boolean getUpdated() {return updated;}
+	public void setProgress(int progress) {
+		this.progress = progress;
+	}
 
-	public void setUpdated(boolean updated) {this.updated = updated;}
-	
-	public int getProgress() {return progress;}
+	public void setActionName(String name) {
+		put("action_name", name);
+	}
 
-	public void setProgress(int progress) {this.progress = progress;}
+	public String getActionName() {
+		return getString("action_name");
+	}
+
+	public String getActionServing() {
+		return getString("serving");
+	}
+
+	public void setActionServing(String serving) {
+		put("serving", serving);
+	}
+
+	public void setActionDesc(String desc) {
+		put("action_desc", desc);
+	}
+
+	public String getActionDesc() {
+		return getString("action_desc");
+	}
+
+	public boolean getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(boolean updated) {
+		this.updated = updated;
+	}
+
 }
